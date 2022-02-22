6536f89905c40432e6d4ce32a023566c590adc5a
Fishappy0 <fishappy0@gmail.com>
2022-02-22 20:04:57 +0700

    Some basic CSS stuff
    22th Feb commit 1% progress, top proportion of the front page is almost sorted out

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,42 +16,50 @@ const intro_buttons = [
 
 const index: NextPage = () => {
     return( 
-        <div className="py-12 bg-white dark:bg-gray-900"> {/* Main container */} 
-            <button onClick={swtich_themes} className='bg-sky-700 font-bold py-1.5 px-3.5 rounded'>Switch Theme</button>
+        <div className="bg-white dark:bg-neutral-900 h-screen min-h-screen"> {/* Main container */} 
             <Head>
                 <title>FBot - Main</title>
                 <meta name="description" content="Discord Bot Manager"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <div className='w-50'> {/* Top title bar with logo and stuff */}
-               <a className="style.title_container" href='..'>
+            <div className='w-50 flex'> {/* Top title bar with logo and stuff */}
+               <div className='flex-auto'/>
+               <a className="flex-auto content-center w-2" href='..'>
                    <link rel="icon" href="/favicon.ico"/>
                    <Image className='content-center'  src='/logo.webp' width={50} height={50}/>
-                   <div className='style.title_text'>FBot</div>
-               </a>
+                   <div className='text-black content-center dark:text-white'>FBot</div>
+                </a>
+                <div className='flex-auto'/>
             </div>
-            <div className="dark:text-white"> {/* Nav bar underneath it when on top page*/}
-                {intro_buttons.map((item) => (
-                    <a
-                        key={item.name}
-                        className={classNames(
-                           'text-black hover:bg-blue-700 hover:text-white', 
-                           'px-3 py-2 rounded-md text-sm font-medium'
-                    )}>
-                            {item.name}
-                    </a>
-                ))}
-                <a className="text-black hover:bg-gray-600 hove:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
-                <a className="px-2"> | </a> 
-                <a className="text-black hover:bg-gray-600 hove:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a> 
-                <div className="style.logo">userlogoplaceholder</div> {/*Replace with dropdown style of google login box*/}
+
+            <div className="dark:text-white flex px-5"> {/* Nav bar underneath it when on top page*/}
+                <div className="about-cluster flex-auto">
+                    {intro_buttons.map((item) => (
+                        <a
+                            key={item.name}
+                            className={classNames(
+                               'text-white hover:bg-neutral-700 hover:text-white', 
+                               'px-3 py-2 rounded-md text-sm font-medium'
+                        )}>
+                                {item.name}
+                        </a>
+                    ))}
+                </div>
+                <div className="login-cluster flex-initial content-end">
+                  <a className="text-white hover:bg-neutral-700 hove:text-white px-3 py-2 rounded-md text-sm font-medium">Register</a>
+                  <a className="px-2"> | </a> 
+                  <a className="text-white hover:bg-neutral-700 hove:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a> 
+                  <a className="style.logo px-2">userlogoplaceholder</a> {/*Replace with dropdown style of google login box*/}
+                </div>
             </div>
+
             <div className="style.slogan">
                 <div className="style.slogan_text">Modular | Dedicated</div>
                 <div className="style.slogan_text">Discord Bot </div>
                 <div className="style.slogan_text">Maker | Host</div>
             </div>
+            <button onClick={swtich_themes} className='bg-neutral-700 font-bold py-1.5 px-3.5 rounded'>Switch Theme</button>
             <div className="style.language_container">
                 <Image src="/language_icon.png" width={20} height={20}/>
                 <div className='style.language_text'>Language</div> {/*Replace with an actual dropdown next plugin*/}
