a6fba16a41746b9a40b8c590ae8d34a70ef50d85
Fishappy0 <fishappy0@gmail.com>
2022-02-28 20:57:51 +0700

    Finishing up the front-page's CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,39 +59,55 @@ const index: NextPage = () => {
                 </div>
                                     
                 <div className='flex-auto text-center text-white text-3xl bg-neutral-800 py-20'> {/*intro cluster */}
-                    <div className="style.slogan_text">Modular | Dedicated</div>
-                    <div className="style.slogan_text">Discord Bot </div>
-                    <div className="style.slogan_text">Maker | Host</div>
+                    <span className=''>
+                        <div className='inline text-sky-400 font-bold px-3'>Modular</div> | 
+                        <div className='inline text-indigo-400 font-bold px-3'>Dedicated </div>
+                    </span>
+                    <div className="font-bold pr-8">Discord Bot </div>
+                    <span className="pr-8">
+                        <div className='inline text-sky-400 font-bold px-3'>Maker</div>| 
+                        <div className='inline text-indigo-400 font-bold px-3'>Host</div>
+                    </span>
+                    <div className="py-10"></div>
+                    <button className='flex-initial bg-sky-400 rounded px-3 py-1 hover:bg-sky-600'>Register Now!</button>
                 </div>
-                                    
-                <div className='flex-auto'>
-                   {/*  <button onClick={swtich_themes} className='bg-neutral-700 font-bold py-1.5 px-3.5 rounded'>Switch Theme</button>*/}
+                {/*                    
                      <div className="style.language_container">
                          <Image src="/language_icon.png" width={20} height={20}/>
-                         <div className='style.language_text'>Language</div> {/*Replace with an actual dropdown next plugin*/}
+                         <div className='style.language_text'>Language</div> Replace with an actual dropdown next plugin
                      </div>
-                     <div className="style.dialouge_box">
-                         <div className="style.dialouge_title">What?</div>
-                         <div className="style.dialogue_text">The quick brown fox jumps over a lazy dog, lorem Ispum.</div>
+                */}
+                <div className='flex flex-col flex-auto text-center bg-neutral-700'>
+                   {/*  <button onClick={swtich_themes} className='bg-neutral-700 font-bold py-1.5 px-3.5 rounded'>Switch Theme</button>*/}
+                     <div className="flex py-4">
+                         <div className="flex-initial pr-6"></div>
+                         <div className="bg-gray-900 rounded px-8 py-5 pr-4 text-white font-bold">
+                             <div className="text-left text-3xl pb-3 text-sky-400">What and Why ?</div>
+                             The quick brown fox jumps over a lazy dog, lorem Ispum.
+                        </div>
+                         <div className="flex-1"></div>
                      </div>
-                     <div className="style.dialouge_box">
-                         <div className="style.dialouge_title">Who?</div>
-                         <div className="style.dialogue_text">The quick brown fox jumps over a lazy dog, lorem Ispum.</div>
+                     <div className="bg-neutral-800 flex py-5">
+                         <div className="flex-auto"></div>
+                         <div className="bg-gray-900 rounded px-8 py-5 text-white font-bold">
+                             <div className="text-center text-4xl pb-3 text-sky-400">Modular ?</div>
+                             The quick brown fox jumps over a lazy dog, lorem Ispum.</div>
+                         <div className="flex-auto"></div>
                      </div>
-                     <div className="style.dialouge_box">
-                         <div className="style.dialouge_title">Why?</div>
-                         <div className="style.dialogue_text">The quick brown fox jumps over a lazy dog, lorem Ispum.</div>
-                     </div>
-                     <div className="style.dialouge_box">
-                         <div className="style.dialouge_title">General information</div>
-                         <div className="style.dialogue_text">The quick brown fox jumps over a lazy dog, lorem Ispum.</div>
+                     <div className="flex py-5">
+                         <div className="flex-auto"></div>
+                         <div className="flex-initial pr-5 bg-gray-900 rounded px-8 py-5 text-white font-bold">
+                             <div className="text-right text-3xl pb text-sky-400">Dedicated ?</div>
+                             The quick brown fox jumps over a lazy dog, lorem Ispum.
+                        </div>
+                        <div className="flex-initial pl-6"></div>
                      </div>
 
-                     <footer className="style.footer">
-                         <div className="style.footer_box">Fishappy0 2022</div>
-                     </footer>
                 </div>
             </div>
+            <footer className="bg-indigo-800">
+                <div className="font-bold text-white pl-5">Fishappy0© 2022</div>
+            </footer>
         </div>
     )
 }
